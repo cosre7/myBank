4d29d6e1d81002e01ecb6fc8f3283639a1d6d407
cosre7 <cosre7@gmail.com>
2021-10-07 11:06:56 +0900

    first commit

---
+++ src/main/java/com/ts/myBank/controller/FixedDepositController.java
@@ -0,0 +1,31 @@
+package com.ts.myBank.controller;
+
+public class FixedDepositController {
+
+	// FixedDepositController가 FixedDepositService에 의존하고있다.
+	private FixedDepositService fixedDepositService;
+	
+	
+//	1단계
+//	/* 
+//	 * FixedDepositController의 생성자는 나중에 자신의 submit 메서드에서 사용할 
+//	 * FixedDepositService의 인스턴스를 만든다.
+//	 */
+//	public FixedDepositController() { 
+//		fixedDepositService = new FixedDepositService();
+//	}
+	
+	public FixedDepositController(FixedDepositService fixedDepositService) {
+		// FixedDepositService 인스턴스가 FixedDepositController 인스턴스의 생성자 인수로 전달 
+		/*
+		 * FixedDepositController를 스프링 빈으로 설정하려면 FixedDepositController 클래스가
+		 * FixedDepositService에 대한 의존관계를 생성자 인수로 받거나 세터 메서드 인수로 받아야 한다. 
+		 */
+		this.fixedDepositService = fixedDepositService;
+	}
+	
+	public boolean submit() {
+		//-- 정기 예금의 상세 정보 저장
+		fixedDepositService.save();
+	}
+}
